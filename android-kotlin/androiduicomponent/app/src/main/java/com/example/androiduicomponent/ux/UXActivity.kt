62b9a4a8bbbc71a1678ca0ce3a718eb916439fd6
Arif Tunçer <arif.61.tuncer@gmail.com>
2025-06-15 01:50:37 +0300

    feat: add ux components

---
+++ android-kotlin/androiduicomponent/app/src/main/java/com/example/androiduicomponent/ux/UXActivity.kt
@@ -0,0 +1,21 @@
+package com.example.androiduicomponent.ux
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androiduicomponent.R
+
+class UXActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_uxactivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
