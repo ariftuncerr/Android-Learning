b93767f4ff9b228b483cbb78341e13429ef01419
Arif Tunçer <arif.61.tuncer@gmail.com>
2025-06-16 17:39:41 +0300

    fix: merge problem

--- android-kotlin/MD3/app/src/main/java/com/example/md3/LaunchActivity.kt
+++ android-kotlin/MD3/app/src/main/java/com/example/md3/LaunchActivity.kt
@@ -1,6 +1,8 @@
 package com.example.md3
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class LaunchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun md3FirstBtnClick (view: View){
+        val intent = Intent(applicationContext, MD3FirstActivity :: class.java)
+        startActivity(intent)
+    }
+    fun md3SecondBtnClick (view: View){
+        val intent = Intent(applicationContext, MD3SecondActivity :: class.java)
+        startActivity(intent)
     }
 }
