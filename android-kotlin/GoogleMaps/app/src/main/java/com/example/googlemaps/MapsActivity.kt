d76449c9931563072620a0c89c9cda3b7bc7fc90
Arif Tunçer <arif.61.tuncer@gmail.com>
2025-06-21 19:49:57 +0300

    camera longClick fixed

--- android-kotlin/GoogleMaps/app/src/main/java/com/example/googlemaps/MapsActivity.kt
+++ android-kotlin/GoogleMaps/app/src/main/java/com/example/googlemaps/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.googlemaps
 
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -23,7 +24,7 @@ import java.security.Permissions
 import java.security.acl.Permission
 import java.util.jar.Manifest
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapLongClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
@@ -32,6 +33,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationListener: LocationListener
 
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean : Boolean? = null
+
+    private var selectedLongitude : Double? = null
+    private var selectedLatitude : Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,18 +52,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         registerPermission()
+        sharedPreferences = this.getSharedPreferences("com.example.googlemaps",MODE_PRIVATE)
+        trackBoolean = false
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        mMap.setOnMapLongClickListener(this)
+
         locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
 
         locationListener = object : LocationListener{
             override fun onLocationChanged(location: Location) {
+
+                /* sürekli çağrıldığı için değişimlerde kamerayı sürekli buraya zoomluyor
                 val userLocation = LatLng(location.latitude, location.longitude)
                 mMap.clear() // Önceki marker'ları temizle
                 mMap.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                */
+                //çözüm -> konum ilk açıldığında bir seferliğe mahsus o konuma gitmek için kayıt tutarız
+                trackBoolean = sharedPreferences.getBoolean("trackBoolean",false)
+                if (trackBoolean == false){
+                    sharedPreferences.edit().putBoolean("trackBoolean",true).apply()
+                    val userLocation = LatLng(location.latitude,location.longitude)
+                    mMap.clear()
+                    mMap.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f))
+                }
+
+
             }
 
             override fun onProviderEnabled(provider: String) {
@@ -80,25 +107,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         else{
             //permission granted
             if (ActivityCompat.checkSelfPermission(this,permission) == PackageManager.PERMISSION_GRANTED){
+
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+                val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (lastLocation != null){
+                    val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+                }
+                mMap.isMyLocationEnabled = true
+
             }
 
+
         }
-        //izin gerekli
 
-
-
-
-        //başlangıç enlem boylam
-        /*val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney")) //açıkalamalı marker ekleyici
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)) //kamerayı bu yöne götür*/
     }
     fun registerPermission(){
         permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if(isGranted){
-                if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (lastLocation != null){
+                        val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+                    }
+                    mMap.isMyLocationEnabled = true
+
+                }
+
             }
             else{
                 Snackbar.make(binding.root,"Permission Needed", Snackbar.LENGTH_SHORT).show()
@@ -106,5 +143,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
         }
+    }
+
+    override fun onMapLongClick(p0: LatLng) {
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(p0))
+        selectedLatitude = p0.latitude
+        selectedLongitude = p0.longitude
     }
 }
