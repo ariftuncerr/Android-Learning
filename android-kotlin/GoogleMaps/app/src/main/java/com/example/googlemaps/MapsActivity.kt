c7d1f824af75db5744a058677c073a1e1b3118ef
Arif Tunçer <arif.61.tuncer@gmail.com>
2025-06-21 06:08:56 +0300

    fix: location permission fixed

--- android-kotlin/GoogleMaps/app/src/main/java/com/example/googlemaps/MapsActivity.kt
+++ android-kotlin/GoogleMaps/app/src/main/java/com/example/googlemaps/MapsActivity.kt
@@ -1,7 +1,15 @@
 package com.example.googlemaps
 
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +18,20 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.googlemaps.databinding.ActivityMapsBinding
+import com.google.android.material.snackbar.Snackbar
+import java.security.Permissions
+import java.security.acl.Permission
+import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var locationManager : LocationManager
+    private lateinit var locationListener: LocationListener
+
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,27 +39,72 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //fragmentte çalıştığı için fragment kurulumu
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        registerPermission()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(location: Location) {
+                val userLocation = LatLng(location.latitude, location.longitude)
+                mMap.clear() // Önceki marker'ları temizle
+                mMap.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+            }
+
+            override fun onProviderEnabled(provider: String) {
+                super.onProviderEnabled(provider)
+            }
+        }
+
+        val permission = android.Manifest.permission.ACCESS_FINE_LOCATION
+
+        if (ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED){
+            //permission denied
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,permission)){
+                Snackbar.make(binding.root,"Permission Needed", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                    permissionLauncher.launch(permission)
+                }.show()
+            }
+            else{
+                permissionLauncher.launch(permission)
+            }
+        }
+        else{
+            //permission granted
+            if (ActivityCompat.checkSelfPermission(this,permission) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+            }
+
+        }
+        //izin gerekli
+
+
+
+
+        //başlangıç enlem boylam
+        /*val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney")) //açıkalamalı marker ekleyici
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)) //kamerayı bu yöne götür*/
+    }
+    fun registerPermission(){
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if(isGranted){
+                if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+            }
+            else{
+                Snackbar.make(binding.root,"Permission Needed", Snackbar.LENGTH_SHORT).show()
+
+            }
+
+        }
     }
 }
