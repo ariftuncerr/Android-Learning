8b77b004c466d35037837057e08cb247b64c7d5f
Arif Tunçer <arif.61.tuncer@gmail.com>
2025-06-23 00:06:20 +0300

    feat(Favorites): Location deleting added

--- android-kotlin/GoogleMaps/app/src/main/java/com/example/googlemaps/adapter/PlaceAdapter.kt
+++ android-kotlin/GoogleMaps/app/src/main/java/com/example/googlemaps/adapter/PlaceAdapter.kt
@@ -5,19 +5,33 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.googlemaps.BitmapByteArrayConverter
+import com.example.googlemaps.FavoritesActivity
 import com.example.googlemaps.MapsActivity
 import com.example.googlemaps.R
 import com.example.googlemaps.database.Place
+import com.example.googlemaps.database.PlaceDao
+import com.example.googlemaps.database.PlaceDatabase
 import com.google.android.material.snackbar.Snackbar
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class PlaceAdapter (private val context: Context, private val placeList : List<Place>)
     :RecyclerView.Adapter<PlaceAdapter.PlaceCardHolder>(){
+        private val compositeDisposable = CompositeDisposable()
+        private lateinit var placeDao : PlaceDao
+        private lateinit var db : PlaceDatabase
 
 
     class PlaceCardHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
@@ -25,6 +39,7 @@ class PlaceAdapter (private val context: Context, private val placeList : List<P
         val placeName = itemView.findViewById<TextView>(R.id.card_placeTxt)
         val placeComment = itemView.findViewById<TextView>(R.id.card_commentTxt)
         val cardItem = itemView.findViewById<CardView>(R.id.cardItem)
+        val deleteBtn = itemView.findViewById<Button>(R.id.card_deleteBtn)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceCardHolder {
@@ -41,6 +56,7 @@ class PlaceAdapter (private val context: Context, private val placeList : List<P
         val currentBitmap = BitmapByteArrayConverter.byteArrayToBitmap(currentByteArray)
         holder.cardImage.setImageBitmap(currentBitmap)
 
+        //go to map
         currentCard.setOnClickListener { view ->
             val intent = Intent(context, MapsActivity :: class.java).apply {
                 putExtra("lat",placeList[position].lat.toDouble())
@@ -52,6 +68,28 @@ class PlaceAdapter (private val context: Context, private val placeList : List<P
                     context.startActivity(intent)
                 }.show()
         }
+
+        val cardDeleteBtn = holder.deleteBtn
+        cardDeleteBtn.setOnClickListener { view ->
+            db = Room.databaseBuilder(context, PlaceDatabase:: class.java,"PlaceDB")
+                .build()
+            placeDao = db.placeDao()
+
+            placeDao.delete(placeList[position])
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()) // uı güncellemesi için
+                .subscribe({
+                    Toast.makeText(context,"Deleted", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(context, FavoritesActivity:: class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    context.startActivity(intent)
+                    }
+                    ,{ error -> println("Silinirken Hata oluştu") })
+
+
+        }
+
+
     }
 
     override fun getItemCount(): Int {
